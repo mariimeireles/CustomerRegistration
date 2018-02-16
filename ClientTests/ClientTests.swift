cee23dc04f8fe20627f829b80950cb9ae603fb4e
mariimeireles <marianaribeiromeireles@gmail.com>
2018-02-15 22:52:46 -0200

    testing inicials generator method

--- ClientTests/ClientTests.swift
+++ ClientTests/ClientTests.swift
@@ -12,26 +12,21 @@ import Nimble
 
 class ClientTests: XCTestCase {
     
+    private var clientUnderTest: ClientModel!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        clientUnderTest = ClientModel(company: "Stone", owner: "Mariana Ribeiro Meireles Gerisztein")
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        clientUnderTest = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_shouldGenerate_threeInicials() {
+        let inicialsExpected = clientUnderTest.generateThreeInicials(name: clientUnderTest.owner)
+        expect(inicialsExpected).to(match("MRM"))
     }
     
 }

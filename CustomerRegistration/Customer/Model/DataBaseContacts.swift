fc09e0d0738dbc3057ebd00757c2527958b836e2
Mariana Meireles <marianaribeiromeireles@gmail.com>
2018-02-21 14:05:53 -0300

    adding core data

---
+++ CustomerRegistration/Customer/Model/DataBaseContacts.swift
@@ -0,0 +1,21 @@
+//
+//  DataBaseContacts.swift
+//  CustomerRegistration
+//
+//  Created by Mariana Meireles | Stone on 2/21/18.
+//  Copyright © 2018 Mariana Meireles. All rights reserved.
+//
+
+import Foundation
+
+
+class DataBaseContacts: RetrieveContacts {
+    
+    func fetchCustomers() -> [Customer] {
+        var customers = [Customer]()
+
+        
+        return customers
+    }
+    
+}

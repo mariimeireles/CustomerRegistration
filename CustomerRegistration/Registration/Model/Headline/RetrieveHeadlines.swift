70bfaa53eba75a0e6bee97264dfe75f85ddaeb9d
Mariana Meireles <marianaribeiromeireles@gmail.com>
2018-02-26 19:06:59 -0300

    Headline model and in memory headline fetcher

---
+++ CustomerRegistration/Registration/Model/Headline/RetrieveHeadlines.swift
@@ -0,0 +1,14 @@
+//
+//  RetrieveHeadlines.swift
+//  CustomerRegistration
+//
+//  Created by Mariana Meireles | Stone on 2/26/18.
+//  Copyright © 2018 Mariana Meireles. All rights reserved.
+//
+
+import Foundation
+
+protocol RetrieveHeadlines {
+    
+    func fetchHeadlines() -> Headline
+}

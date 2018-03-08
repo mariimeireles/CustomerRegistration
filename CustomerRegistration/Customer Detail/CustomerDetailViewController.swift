da5f82bf5579f9a5155a551d09e2f9b52de40a03
Mariana Meireles <marianaribeiromeireles@gmail.com>
2018-03-08 11:51:41 -0300

    making phone call

--- CustomerRegistration/Customer Detail/CustomerDetailViewController.swift
+++ CustomerRegistration/Customer Detail/CustomerDetailViewController.swift
@@ -13,7 +13,7 @@ class CustomerDetailViewController: UIViewController {
     @IBOutlet weak var companyLabel: UILabel!
     @IBOutlet weak var ownerLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
-    @IBOutlet weak var telephoneLabel: UILabel!
+    @IBOutlet weak var telephoneButton: UIButton!
     @IBOutlet weak var cnpjLabel: UILabel!
     @IBOutlet weak var activeSinceLabel: UILabel!
     @IBOutlet weak var meiLabel: UILabel!
@@ -28,7 +28,7 @@ class CustomerDetailViewController: UIViewController {
         self.companyLabel.text = customer.companyName
         self.ownerLabel.text = customer.ownerName
         self.emailLabel.text = customer.email
-        self.telephoneLabel.text = customer.telephone
+        self.telephoneButton.setTitle(customer.telephone, for: .normal)
         self.cnpjLabel.text = customer.cnpj
         self.activeSinceLabel.text = customer.activeSince
         if customer.isMei == true {
@@ -37,5 +37,14 @@ class CustomerDetailViewController: UIViewController {
             self.meiLabel.text = "No"
         }
     }
+    
+    @IBAction func phoneButton(_ sender: Any) {
+        let phone = customer.telephone
+        let formattedPhone = phone?.replacingOccurrences(of: "[ ()-]", with: "", options: [.regularExpression])
+        guard let number = URL(string: "tel://" + formattedPhone!) else { return }
+        UIApplication.shared.open(number)
+    }
+    
+    
 
 }
